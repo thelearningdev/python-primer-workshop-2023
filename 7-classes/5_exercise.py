6d54450949fbb057242b0778bbb9279ddd10bd1d
bhavaniravi <bhava0895@gmail.com>
2023-04-04 23:35:17 +0530

    add exercise to classes

---
+++ 7-classes/5_exercise.py
@@ -0,0 +1,26 @@
+"""
+1. Create a Book class with attributes title, author, publisher, genre, and price.
+
+2. Add a method to the Book class called discount that takes a percentage as an argument and returns the discounted price of the book.
+
+3. Create an instance of the Book class and test the discount method.
+
+4. Create a Fiction subclass of Book that adds an attribute called plot and a method called print_plot that prints the plot of the book.
+
+5. Create an instance of the Fiction class and test the print_plot method.
+
+6. Create a NonFiction subclass of Book that adds an attribute called topic and a method called print_topic that prints the topic of the book.
+
+7. Create an instance of the NonFiction class and test the print_topic method.
+"""
+
+# Advanced, To attempt the following exercises, you need to keep track of all the book objects created
+# On saving or reading from the file, the objects need to be created and returned
+
+"""
+8. Add a method to the Book class called save_to_file that saves the book's attributes to a CSV file.
+
+9. Add a classmethod to the Book class called load_from_file that loads a list of Book objects from a CSV file.
+
+10. Test the save_to_file and load_from_file methods by creating a list of Book objects, saving it to a CSV file, and then loading it back into a list of objects.
+"""
