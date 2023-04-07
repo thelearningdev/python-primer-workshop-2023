1610b8a37e201bb1adc79d4b21887043c110eb37
bhavaniravi <bhava0895@gmail.com>
2023-04-07 19:25:30 +0530

    Add data model exercise

---
+++ 10-data-model/exercises/2_data_driven.py
@@ -0,0 +1,5 @@
+# Check this file 2-conditionals/2_match.py
+
+# Is there a data driven way of achieving this?
+
+# Hint: When data driven we are thinking of using a data structure to store the data and then retrive it dynamically based on input
