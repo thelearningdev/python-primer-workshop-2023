80873178eb979ead3690d4127a5ea72c3ec174a7
bhavaniravi <bhava0895@gmail.com>
2023-04-08 11:36:55 +0530

    rename exercises

--- 2-conditionals/exercises/2_use_and_or.py
+++ 2-conditionals/exercises/2_use_and_or.py
@@ -1,4 +1,5 @@
 # Rewrite the following to use `and` and `or` conditional experession
+# The goal is to have only if and else block
 
 if x == 2:
     print("x is 2")
--- 3-iteration/exercises/2_nearest_even.py
+++ 3-iteration/exercises/2_nearest_even.py
@@ -1,3 +1,5 @@
 x = [1, 3, 5, 7, 9, 11]
 
 # Given the above list use a for loop and print the nearest even number
+# Output: 2, 4, 6, 8, 10.
+# Print in any format you want
