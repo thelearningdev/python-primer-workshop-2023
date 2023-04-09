5cb78c507f78bc5537c5a1c14b0bb6ea9b3b2520
bhavaniravi <bhava0895@gmail.com>
2023-04-09 14:06:18 +0530

    add sets

---
+++ 5-collection-data-types/4-sets/1_sets.py
@@ -0,0 +1,13 @@
+# empty set
+
+
+# set with values
+
+
+# add items to set
+
+
+# remove items from set
+
+
+# Set operations
---
+++ 5-collection-data-types/4-sets/2_set_methods.py
@@ -0,0 +1,10 @@
+s1 = {1, 2, 3, 4, 5}
+s2 = {2, 3, 4, 7}
+
+s1.remove(2)
+s1.pop()
+s1.intersection(s2)
+s1.disjoint(s2)
+s1.union(s2)
+
+
