dda843cdcc0e69391637489170022cf0c1a5788b
bhavaniravi <bhava0895@gmail.com>
2023-04-11 12:07:04 +0530

    day 2

--- 6-file_handling/1_file_handling.py
+++ 6-file_handling/1_file_handling.py
@@ -1,6 +1,6 @@
 # Creating a file object
 
-f = open("data/books.csv, "r")
+f = open("data/books.csv", "r")
 
 
 # Reading a file
@@ -11,16 +11,20 @@ print (f.read())
 # Writing to a File
 
 f = open("data/new.txt", "w")
-file.write("A new file is created an new data is entered")
-
+f.write("A new file is created an new data is entered")
+f.close()
 
 # Appending data to a file
 
-f = open("data/books.csv", "a")
-file.write("A new file is created an new data is entered")
+f = open("data/new.txt", "a")
+f.write("A new file is created an new data is entered")
+f.close()
 
 # File seek
 
-f = open("data/books.csv, "r")
+f = open("data/books.csv", "r")
 print (f.read())
 print (f.read()) # what's happening here
+
+f.seek(0)
+print (f.read())
